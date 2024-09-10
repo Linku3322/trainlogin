f598665d6d58f63e4073e276b81bfd9c181e8f1b
Linku3322 <smrutibarik3@gmail.com>
2024-09-10 12:51:12 +0530

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,10 @@
+let moviename = "spider man";
+var moviedescription = "book";
+const price = 300;
+console.log(moviename)
+console.log(moviedescription)
+console.log(price)
+let x = 10;
+    y=20;
+console.log(x<y)
+console.log(x<=y)    
